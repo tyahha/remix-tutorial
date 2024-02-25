d396f7a1ee8e6e71f298318cfb5422d992753f88
toshinori.sato <tyahhahha@gmail.com>
2024-02-25 23:17:22 +0900

    check params

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,21 @@
-import { Form } from "@remix-run/react";
+import {json, LoaderFunctionArgs} from "@remix-run/node";
+import {Form, useLoaderData} from "@remix-run/react";
 import type { FunctionComponent } from "react";
 
-import type { ContactRecord } from "~/data";
+import {ContactRecord, getContact} from "~/data";
+import invariant from "tiny-invariant";
+
+export const loader = async ({params}: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact });
+}
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact} = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
