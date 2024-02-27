998bd3b2d7008879bbca3784b65231f9d6006911
toshinori.sato <tyahhahha@gmail.com>
2024-02-27 22:42:03 +0900

    add action

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import appStylesHref from "./app.css";
-import { getContacts } from "~/data";
+import {createEmptyContact, getContacts} from "~/data";
 
 export const loader = async () => {
   const contacts = await getContacts();
@@ -21,6 +21,11 @@ export const loader = async () => {
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+}
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
