02c174e8202341b0d7784bea8ed8ef1fde22d02f
toshinori.sato <tyahhahha@gmail.com>
2024-03-25 22:23:59 +0900

    sync url and form state

--- app/root.tsx
+++ app/root.tsx
@@ -12,12 +12,13 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 import appStylesHref from "./app.css";
 import {createEmptyContact, getContacts} from "~/data";
+import {useEffect} from "react";
 
 export const loader = async ({request}: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const q = url.searchParams.get('q');
   const contacts = await getContacts(q);
-  return json({ contacts });
+  return json({ contacts, q });
 }
 
 export const links: LinksFunction = () => [
@@ -30,8 +31,15 @@ export const action = async () => {
 }
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <html lang="en">
@@ -49,6 +57,7 @@ export default function App() {
               <input
                 id="q"
                 aria-label="Search contacts"
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
